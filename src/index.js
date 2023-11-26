744b914af32f434afc1701d2640e0f750b21da85
Abhishek Shankar Choudhary <theabhishek.srm@gmail.com>
2023-11-26 15:29:56 +0530

    removed code

--- src/index.js
+++ src/index.js
@@ -5,7 +5,19 @@ import "./style.css"
 // JSX - JavaScript XML
 
 
+function Hello(){
 
+    let name = "Abhishek" 
+ 
+    let h1Style = {textAlign: "center", border: "2px solid red"}
+ 
+     return (
+            <div className="hello">
+               <h1 style={h1Style}>Hello {name} {2+3+5+9}</h1>
+               <p>Hey this is my first React App</p>
+            </div>
+            )
+ }
 
 ReactDOM.render(<Hello/>, document.getElementById("root"));
 
